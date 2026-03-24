ba2d4d7e50cf57c2cf8ed501d4fc758ddf4dd296
Raffaele D'Alterio <r.dalterio@almaviva.it>
2026-03-24 14:35:27 +0100

    fix: rm dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   { path: '', redirectTo: 'quiz', pathMatch: 'full' },
   { path: 'quiz', loadComponent: () => import('./quiz/quiz.component').then(m => m.QuizComponent) },
-  { path: 'dashboard', loadComponent: () => import('./dashboard/dashboard.component').then(m => m.DashboardComponent) },
   { path: 'errors', loadComponent: () => import('./errors/errors.component').then(m => m.ErrorsComponent) },
   { path: 'progressi', loadComponent: () => import('./progressi/progressi.component').then(m => m.ProgressiComponent) },
-  { path: 'utenti', loadComponent: () => import('./utenti/utenti.component').then(m => m.UtentiComponent) },
-
+  { path: 'utenti', loadComponent: () => import('./utenti/utenti.component').then(m => m.UtentiComponent) }
 ];
